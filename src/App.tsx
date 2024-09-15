93a85c7d783246aba218455206305470ae1ad551
Sam Watkinson <samwatkinson@outlook.com>
2024-09-15 19:55:55 +0100

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="container mx-auto">
+            <p>Hello world!</p>
+        </div>
+    );
+}
+
+export default App;
